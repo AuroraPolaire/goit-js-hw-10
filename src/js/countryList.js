988e7a3457b0af7cb2cdad29c01d64f73ae30cdd
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-01-19 15:10:28 +0200

    icons

--- src/js/countryList.js
+++ src/js/countryList.js
@@ -1,7 +1,7 @@
 export const createCountryList = ({ flags, name }) => {
   return `
         <li class="country-list__item">
-        <img class="country-list__svg" src='${flags.svg}' alt='Flag of ${name.common}' width='40px'><span class="country-list__name"> ${name.official}</span>
+        <span class="county-list__svg-box"><img class="country-list__svg" src='${flags.svg}' alt='Flag of ${name.common}' width='40px'></span><span class="country-list__name"> ${name.official}</span>
         </li>
 `;
 };
